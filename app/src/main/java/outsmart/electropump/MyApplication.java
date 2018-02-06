e257127ee302f5597d4e7f8bf941e047f8ca0116
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-06 01:26:33 -0200

    Using jitpack

--- app/src/main/java/outsmart/electropump/MyApplication.java
+++ app/src/main/java/outsmart/electropump/MyApplication.java
@@ -3,6 +3,7 @@ package outsmart.electropump;
 import android.app.Application;
 
 import com.walmartlabs.ern.container.ElectrodeReactContainer;
+import com.walmartlabs.ern.container.plugins.CodePushPlugin;
 
 /**
  * Created by rudi eros on 2/4/2018.
@@ -13,6 +14,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         ElectrodeReactContainer.initialize(this,
-                new ElectrodeReactContainer.Config().isReactNativeDeveloperSupport(BuildConfig.DEBUG));
+                new ElectrodeReactContainer.Config().isReactNativeDeveloperSupport(BuildConfig.DEBUG),
+                new CodePushPlugin.Config("Ihnqwl-q2GeVHXXowPkZN4uMlUvU1e0b1580-1e9f-4dc6-a7c9-4c6f6ac0bec5"));
     }
 }
